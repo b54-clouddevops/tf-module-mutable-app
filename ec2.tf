c9ef47831fb0e9e81f223f0433796abb9fdc0fde
CodingManoj <mvp2612@hotmail.com>
2023-08-05 22:25:24 -0400

    Git Auto Push

--- ec2.tf
+++ ec2.tf
@@ -40,13 +40,15 @@ resource "aws_instance" "od" {
 #   }
 }
 
-
+locals {
+    INSTANCE_IDS    =   concat(aws_spot_instance_request.spot.*.spot_instance_id, aws_instance.od.*.id)
+}
 
 # Creating tag and attaching it to the Instances 
 resource "aws_ec2_tag" "tags" {
   count       = var.OD_INSTANCE_COUNT + var.SPOT_INSTANCE_COUNT
 
-  resource_id = concat(aws_spot_instance_request.spot.*.spot_instance_id, aws_instance.od.*.id)
+  resource_id = element(local.INSTANCE_IDS, count.index)
   key         = "Name"
   value       = "${var.COMPONENT}-${var.ENV}"
 }
