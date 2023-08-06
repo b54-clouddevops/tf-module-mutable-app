d776f27129c313239baba3878b670b340940314b
CodingManoj <mvp2612@hotmail.com>
2023-08-05 21:50:25 -0400

    Git Auto Push

---
+++ data.tf
@@ -0,0 +1,18 @@
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+  config = {
+    bucket = "b54-tf-remote-state"
+    key    = "vpc/${var.ENV}/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+
+data "terraform_remote_state" "alb" {
+  backend = "s3"
+  config = {
+    bucket = "b54-tf-remote-state"
+    key    = "alb/${var.ENV}/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
