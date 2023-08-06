0e10e43e50f841cc89bfc9240a957b304bdaa01a
CodingManoj <mvp2612@hotmail.com>
2023-08-05 22:32:18 -0400

    Git Auto Push

--- appDeploy.tf
+++ appDeploy.tf
@@ -12,7 +12,7 @@ resource "null_resource" "app_deploy" {
         }
 
         inline = [
-             hostname
+             "hostname"
            //"ansible-pull -U https://github.com/b54-clouddevops/ansible.git -e ENV=dev -e COMPONENT=${var.COMPONENT} -e APP_VERSION=${var.APP_VERSION} roboshop-pull.yml"
         ]
     }
