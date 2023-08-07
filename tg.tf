54cf83efccab46e0e72e4fc7217edd87a9c071fe
CodingManoj <mvp2612@hotmail.com>
2023-08-06 21:29:33 -0400

    Git Auto Push

--- tg.tf
+++ tg.tf
@@ -15,4 +15,20 @@ resource "aws_lb_target_group_attachment" "attach_instances" {
   port             = 8080
 }
 
-# Attach the TargetGroup to the ALB ( frontend-tg should be attached to Public ALB and backend-tg's should go and attach to Public ALB) 
+
+# Creates Rule In the Listener
+resource "aws_lb_listener_rule" "app_rule" {
+  listener_arn = data.terraform_remote_state.alb.outputs.PRIVATE_LISTENER_ARN 
+  priority     = 100
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app.arn
+  }
+
+  condition {
+    host_header {
+      values = ["${var.COMPONENT}-${var.ENF}.data.terraform_remote_state.alb.outputs.PRIVATE_HOSTED_ZONE_NAME "]
+    }
+  }
+}
