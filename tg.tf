7b5984731879b703689bfc2f201f33564fc3bcec
CodingManoj <mvp2612@hotmail.com>
2023-08-07 21:34:59 -0400

    Git Auto Push

--- tg.tf
+++ tg.tf
@@ -25,7 +25,7 @@ resource "aws_lb_target_group_attachment" "attach_instances" {
 
 # Generates Random Number 
 resource "random_integer" "priority" {
-    max = 100
+    max = 105
     min = 500
 }
 
