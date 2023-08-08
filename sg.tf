0556746482d67505a07442a8f28f460f4b15cbee
CodingManoj <mvp2612@hotmail.com>
2023-08-07 21:39:07 -0400

    Git Auto Push

--- sg.tf
+++ sg.tf
@@ -19,6 +19,14 @@ resource "aws_security_group" "allows_app" {
     cidr_blocks = [data.terraform_remote_state.vpc.outputs.VPC_CIDR , data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_CIDR]
   }
 
+  ingress {
+    description = "HC Port"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = [data.terraform_remote_state.vpc.outputs.VPC_CIDR , data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_CIDR]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
