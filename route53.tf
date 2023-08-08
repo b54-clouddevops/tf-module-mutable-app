5b66b81425f955192cbdf3b0b823469d7ee0a40f
CodingManoj <mvp2612@hotmail.com>
2023-08-07 20:46:12 -0400

    Git Auto Push

--- route53.tf
+++ route53.tf
@@ -1,8 +1,8 @@
 resource "aws_route53_record" "dns_record" {
-  zone_id = var.LB_TYPE == internal ? data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_ID : data.terraform_remote_state.vpc.outputs.PUBLIC_HOSTED_ZONE_ID 
+  zone_id = var.LB_TYPE == "internal" ? data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_ID : data.terraform_remote_state.vpc.outputs.PUBLIC_HOSTED_ZONE_ID 
 
   name    = "${var.COMPONENT}-${var.ENV}"
   type    = "CNAME"
   ttl     = 10
-  records = var.LB_TYPE == internal ? [data.terraform_remote_state.alb.outputs.PRIVATE_ALB_ADDRES ] : [data.terraform_remote_state.alb.outputs.PUBLIC_ALB_ADDRESS]
+  records = var.LB_TYPE == "internal" ? [data.terraform_remote_state.alb.outputs.PRIVATE_ALB_ADDRES ] : [data.terraform_remote_state.alb.outputs.PUBLIC_ALB_ADDRESS]
 }
