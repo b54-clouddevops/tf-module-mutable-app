6a3daea0b2d6c8f74c4cbc3f0c477157bfc98354
CodingManoj <mvp2612@hotmail.com>
2023-08-08 20:55:23 -0400

    Git Auto Push

--- tg.tf
+++ tg.tf
@@ -31,6 +31,8 @@ resource "random_integer" "priority" {
 
 # Creates Rule In the Listener
 resource "aws_lb_listener_rule" "app_rule" {
+  count            = var.LB_TYPE == "internal" ? 1 : 0
+
   listener_arn = data.terraform_remote_state.alb.outputs.PRIVATE_LISTENER_ARN 
   priority     = random_integer.priority.result
 
